4d9fb6feef8d817a36b56fe4c85c22616273d4e9
justinbomber <w0913706494@gmail.com>
2025-11-05 11:47:51 +0800

    Update database.py

--- embedding/db_summary_extractor/infrastructure/database.py
+++ embedding/db_summary_extractor/infrastructure/database.py
@@ -123,8 +123,10 @@ class SupabaseSummaryRepository(SummaryRepository):
             "point_id": lock_point_id,
             "jid": jid,
             "jdate": jdate_int,
-            "summary_type": "lock",
+            # "summary_type": "lock",
             "content": "",
+            "embedded_1": True,
+            "embedded_2": True
         }
         self.client.schema(self.schema_name).table("judgment_summary").upsert(record_data).execute()
     
